6a501312c5a02b5e65e65d02588446f0cc916808
Maud Bay <maudbay@users.noreply.github.com>
2017-04-05 11:15:24 +0200

    Create index-github.php

---
+++ index-github.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>HEC Liege</title>
+</head>
+<body>
+//created on git hub
+<header>
+   <h1>Test Git</h1>
+</header>
+<section>
+<?php
+echo "Hello World";	
+?>
+</section>
+
+<footer>HEC Li&egrave;ge  UDI-NTE 2017</footer>
+
+
+
+</body>
+</html>
+
